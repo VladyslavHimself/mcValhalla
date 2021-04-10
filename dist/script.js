5b784dcd8cd1396fb2ae2b22e68aff0b96ee16d6
VladyslavHimself <vladyslavhimself@gmail.com>
2021-04-10 22:29:22 +0300

    init & created preloader

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preloader');
+    preload.classList.add('finishPreload');
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+});
+
