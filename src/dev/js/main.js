7b2e3734cffe2da7acbae7758cea1704a8f69246
VladyslavHimself <vladyslavhimself@gmail.com>
2021-07-11 00:30:05 +0300

    added other static pages & refreshed route

--- src/dev/js/main.js
+++ src/dev/js/main.js
@@ -13,7 +13,24 @@ window.addEventListener('load', (e) => {
     addClassToSelector('.welcome', 'readyToAnimation');
 });
 
+function initializeBurger() {
 
+    setTimeout(() => {
+        const burger = document.querySelector('.burger'),
+        navigation = document.querySelector('.navigation');
+  
+        // navigation
+      burger.addEventListener('click', () => {
+          navigation.style.transform = 'translate(-100%, 0)';
+          
+      })
+
+      document.querySelector('.navigation__close').addEventListener('click', () => {
+          navigation.style.transform = 'translate(0%, 0%)';
+      })
+      console.log('all init!')    
+    }, 500);
+}
 
 barba.hooks.afterEnter((data) => {
 
@@ -24,23 +41,12 @@ barba.hooks.afterEnter((data) => {
 
     if (data.next.url.href.match('./html/home.html')) {
 
-        const burger = document.querySelector('.burger'),
-              navigation = document.querySelector('.navigation');
         
-              // navigation
-            burger.addEventListener('click', () => {
-                navigation.style.transform = 'translate(-100%, 0)';
-                
-            })
-
-            document.querySelector('.navigation__close').addEventListener('click', () => {
-                navigation.style.transform = 'translate(0%, 0%)';
-            })
-
+            initializeBurger();
 
             const textIp = "mc.teaparty.fun";
             const playButton = document.querySelector('.playButton');
-    
+
             playButton.addEventListener('click', () => {
                 navigator.clipboard.writeText(textIp).then(function() {
                     document.getElementById('btnText').innerHTML = 'IP COPIED TO CLIPBOARD!';
@@ -52,9 +58,23 @@ barba.hooks.afterEnter((data) => {
                     console.error('Async: Could not copy text: ', err);
                 });
             });
-    
 
+            
+    }
 
+    if (data.next.url.href.match('./html/about.html')) {
+        console.log('intialized');
+        initializeBurger();            
+    }
+
+    if (data.next.url.href.match('./html/join.html')) {
+        console.log('intialized');
+        initializeBurger();            
+    }
+
+    if (data.next.url.href.match('./html/team.html')) {
+        console.log('intialized');
+        initializeBurger();            
     }
     
 });
