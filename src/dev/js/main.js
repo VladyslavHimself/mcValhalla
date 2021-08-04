ae663c9fe316b422a4f2b3d3b459e3aaede358fb
Vladyslav Lutchyn <vladyslavhimself@Vladyslavs-MacBook-Pro.local>
2021-08-03 21:35:54 -0400

    fixed js trash

--- src/dev/js/main.js
+++ src/dev/js/main.js
@@ -5,28 +5,25 @@ const { default: addClassToSelector } = require('./functions/addClassToSelector'
 
 window.addEventListener('load', () => {
   // finish preloader
-  addClassToSelector('.preloader', 'finishPreload')
+  addClassToSelector('.preloader', 'finishPreload');
   // finish intro
-  addClassToSelector('.welcome', 'readyToAnimation')
+  addClassToSelector('.welcome', 'readyToAnimation');
 });
 
+// Initialize burger
 function initializeBurger() {
+  setTimeout(() => {
+    const burger = document.querySelector('.burger');
+    const navigation = document.querySelector('.navigation');
+    // navigation
+    burger.addEventListener('click', () => {
+      navigation.style.transform = 'translate(-100%, 0)';
+    });
 
-    setTimeout(() => {
-        const burger = document.querySelector('.burger'),
-        navigation = document.querySelector('.navigation');
-  
-        // navigation
-      burger.addEventListener('click', () => {
-          navigation.style.transform = 'translate(-100%, 0)';
-          
-      })
-
-      document.querySelector('.navigation__close').addEventListener('click', () => {
-          navigation.style.transform = 'translate(0%, 0%)';
-      })
-      console.log('all init!')    
-    }, 500);
+    document.querySelector('.navigation__close').addEventListener('click', () => {
+      navigation.style.transform = 'translate(0%, 0%)';
+    });
+  }, 500);
 }
 
 barba.hooks.afterEnter((data) => {
@@ -35,47 +32,36 @@ barba.hooks.afterEnter((data) => {
     addClassToSelector('.welcome', 'readyToAnimation');
   }
 
-    if (data.next.url.href.match('/index.html')) {
-        addClassToSelector('.preloader', 'finishPreload');
-        addClassToSelector('.welcome', 'readyToAnimation');
-    }
+  if (data.next.url.href.match('/index.html')) {
+    addClassToSelector('.preloader', 'finishPreload');
+    addClassToSelector('.welcome', 'readyToAnimation');
+  }
 
-    if (data.next.url.href.match('./html/home.html')) {
+  if (data.next.url.href.match('./html/home.html')) {
+    initializeBurger();
 
-        
-            initializeBurger();
+    const textIp = 'mc.teaparty.fun';
+    const playButton = document.querySelector('.playButton');
 
-            const textIp = "mc.teaparty.fun";
-            const playButton = document.querySelector('.playButton');
+    playButton.addEventListener('click', () => {
+      navigator.clipboard.writeText(textIp).then(() => {
+        document.getElementById('btnText').innerHTML = 'IP COPIED TO CLIPBOARD!';
+        setTimeout(() => {
+          document.getElementById('btnText').innerHTML = 'Start play on Teaparty';
+        }, 3000);
+      });
+    });
+  }
 
-            playButton.addEventListener('click', () => {
-                navigator.clipboard.writeText(textIp).then(function() {
-                    document.getElementById('btnText').innerHTML = 'IP COPIED TO CLIPBOARD!';
-                    setTimeout(() => {
-                        document.getElementById('btnText').innerHTML = 'Start play on Teaparty';
-                    }, 3000);
-                    console.log('Succesfully');
-                }, function(err) {
-                    console.error('Async: Could not copy text: ', err);
-                });
-            });
+  if (data.next.url.href.match('./html/about.html')) {
+    initializeBurger();
+  }
 
-            
-    }
+  if (data.next.url.href.match('./html/join.html')) {
+    initializeBurger();
+  }
 
-    if (data.next.url.href.match('./html/about.html')) {
-        console.log('intialized');
-        initializeBurger();            
-    }
-
-    if (data.next.url.href.match('./html/join.html')) {
-        console.log('intialized');
-        initializeBurger();            
-    }
-
-    if (data.next.url.href.match('./html/team.html')) {
-        console.log('intialized');
-        initializeBurger();            
-    }
-    
+  if (data.next.url.href.match('./html/team.html')) {
+    initializeBurger();
+  }
 });
